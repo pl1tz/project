35ad1e2696bac41b66cdde646d68660baee217c2
pl1tz <pl1tz2021@gmail.com>
2025-01-10 14:46:54 +0300

    fix:update all catalog

--- app/services/car_catalog_service.rb
+++ app/services/car_catalog_service.rb
@@ -2,11 +2,22 @@ class CarCatalogService
 
   def self.all_catalog
     CarCatalog.all.group_by { |car| car.brand }.sort.to_h.map do |brand, cars|
-        {
-          brand: brand,
-          models_count: cars.uniq { |car| car.model }.count,
-          models: cars.map { |car| { id: car.id, model: car.model } }.uniq.sort_by { |car| car[:model] }
-        }
+      {
+        brand: brand,
+        cars: cars.map do |car|
+          {
+            id: car.id,
+            brand: car.brand,
+            model: car.model,
+            power: car.power,
+            acceleration: car.acceleration,
+            consumption: car.consumption,
+            car_color: car.car_colors.first ? { id: car.car_colors.first.id, image: car.car_colors.first.image } : nil
+          }
+        end,
+        models_count: cars.uniq { |car| car.model }.count,
+        models: cars.map { |car| { id: car.id, model: car.model } }.uniq.sort_by { |car| car[:model] }
+      }
     end
   end
 
