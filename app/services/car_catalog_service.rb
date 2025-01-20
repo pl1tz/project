f61b94fbabd095494ff64791027fe04d1d3e58c8
pl1tz <pl1tz2021@gmail.com>
2025-01-20 17:27:31 +0300

    usecar update all 4

--- app/services/car_catalog_service.rb
+++ app/services/car_catalog_service.rb
@@ -1,25 +1,30 @@
 class CarCatalogService
 
   def self.all_catalog
-    CarCatalog.all.group_by { |car| car.brand }.sort.to_h.map do |brand, cars|
-      {
-        brand: brand,
-        cars: cars.map do |car|
-          {
-            id: car.id,
-            brand: car.brand,
-            model: car.model,
-            power: car.power,
-            acceleration: car.acceleration,
-            consumption: car.consumption,
-            car_color: car.car_colors.first ? { id: car.car_colors.first.id, image: car.car_colors.first.image } : nil,
-            car_catalog_configuration_price: car.car_catalog_configurations.first&.special_price
-          }
-        end,
-        models_count: cars.uniq { |car| car.model }.count,
-        models: cars.map { |car| { id: car.id, model: car.model } }.uniq.sort_by { |car| car[:model] }
-      }
-    end
+    CarCatalog.includes(:car_colors, :car_catalog_configurations)
+              .all
+              .group_by(&:brand)
+              .sort
+              .to_h
+              .map do |brand, cars|
+                {
+                  brand: brand,
+                  cars: cars.map do |car|
+                    {
+                      id: car.id,
+                      brand: car.brand,
+                      model: car.model,
+                      power: car.power,
+                      acceleration: car.acceleration,
+                      consumption: car.consumption,
+                      car_color: car.car_colors.first ? { id: car.car_colors.first.id, image: car.car_colors.first.image } : nil,
+                      car_catalog_configuration_price: car.car_catalog_configurations.first&.special_price
+                    }
+                  end,
+                  models_count: cars.uniq(&:model).count,
+                  models: cars.map { |car| { id: car.id, model: car.model } }.uniq.sort_by { |car| car[:model] }
+                }
+              end
   end
 
     def self.cars_by_brand(brand_name)
