7c46af4c27828d4b8c2a4d9d729b20e1a31979d4
angryfronter <danil.huzarevich@gmail.com>
2025-03-21 10:05:12 +0300

    youauto-credit.ru

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,8 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "youautoplus.ru"
-  config.hosts << "www.youautoplus.ru"
+  config.hosts << "youauto-credit.ru"
+  config.hosts << "www.youauto-credit.ru"
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
